dcf29cc7856c7c14fa3880f863fc1c9a5c03dbb4
Rita Mernissi <mernissi.rita@gmail.com>
2018-10-27 16:12:09 +0200

    move files to src

---
+++ src/main.js
@@ -0,0 +1,22 @@
+var board= 
+    [[0,0,0,bomb,bomb             ],
+     [startPoint,0,0,bomb,endPoint],
+     [bomb,0,bomb,0,0             ],
+     [0,0,0,0,0                   ],
+     [0,0,0,bomb,0                ]];
+
+
+// cases 
+var bomb= {
+  this.image= //url de mon image
+}
+
+/*
+//  celulle (3,4) ==> matrix[2][3]
+function getPosition(matrix, [row,column]) {
+    var r= row-1;
+    var c= column-1;
+    return matrix[r][c];
+  };
+
+*/
