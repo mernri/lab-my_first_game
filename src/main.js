a4f0178dd3a83b0e1ef283f64adaa0ebe2cbe798
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-04 23:23:02 +0100

    problem : can't select a specific cell to change its class (JQuery)

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
 //__________ CREATE A MATRIX WITH BOMBS, END/START POINTS ___________________
 
-function matrixCreate (board) {
+var matrixCreate = board => {
   for (var i= 0; i < board.length; i++){
     var row = $("<div></div>");
     $("#game_board").append(row);
@@ -48,31 +48,30 @@ function matrixCreate (board) {
 
 // _____________ FUNCTIONS FOR MOVING _____________________________________
 
-// var i= 1;
-// var j= 0; 
-// var position = board[i][j];
+var i= 1;
+var j= 0; 
+var position = board[i][j];
 
 
-// function updatePosition() {
+var moveRight = key => (j < 4 ? j+= 1 : j)
+var moveLeft = key => (j > 0 ? j-= 1 : j)
+var moveUp = key => (i > 0 ? i-= 1 : i)
+var moveDown = key => (i < 4 ? i+= 1 : i)
 
-// } 
-
-var moveRight = key => (i < 4 ? i+= 1 : i)
-var moveLeft = key => (i > 0 ? i-= 1 : i)
-var moveUp = key => (j > 0 ? j-= 1 : j)
-var moveDown = key => (j < 4 ? j+= 1 : j)
-
-// document.onkeydown = function(key) {
-//   switch (key.keyCode) {
-//     case 37: position.moveLeft();
-//     break;
-//     case 39: position.moveRight();
-//     break;
-//     case 38: position.moveUp();
-//     break;
-//     case 40: position.moveDown();
-//     break;
-//   }
-//   updatePosition();
-// };
+document.onkeydown = function(key) {
+  switch (key.keyCode) {
+    case 37: moveLeft();
+    break;
+    case 39: moveRight();
+    alert("la nouvelle position est : (" + i + "," + j + ")");
+    break;
+    case 38: moveUp();
+    break;
+    case 40: moveDown();
+    break;
+  }
+  // UPDATE CELL COLOR => DOESN'T WORK
+  var cell = $('<div class="cell ' + board[i][j] +'"> </div>');
+    cell.addClass("used");
+};
 
