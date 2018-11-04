a203f0f6db437026cc036a5e5f149a716f02aae3
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-04 20:38:31 +0100

    moveLeft moveRight moveUp moveDown functions added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 // cases
 var bomb = "bomb";
-
 var light = "light";
-
+var free = "free";
 
 var board = [
-  [0, 0, 0, bomb, bomb],
-  [light, 0, 0, bomb, light],
-  [bomb, 0, bomb, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, bomb, 0]
+  [free, free, free, bomb, bomb],
+  [light, free, free, bomb, light],
+  [bomb, free, bomb, free, free],
+  [free, free, free, free, free],
+  [free, free, free, bomb, free]
 ];
 
 
@@ -23,6 +22,7 @@ function matrixCreate (board) {
   for (var i= 0; i < board.length; i++){
     var row = $("<div></div>");
     $("#game_board").append(row);
+
     for (var j= 0; j < board[i].length; j++){
       var cell = $('<div class="cell ' + board[i][j] +'"> </div>');
       row.append(cell);
@@ -40,6 +40,39 @@ function matrixCreate (board) {
           $(".bomb").addClass("hidden");         
         }, 3000);
       }
+
     }
   }
 }
+
+
+// _____________ FUNCTIONS FOR MOVING _____________________________________
+
+// var i= 1;
+// var j= 0; 
+// var position = board[i][j];
+
+
+// function updatePosition() {
+
+// } 
+
+var moveRight = key => (i < 4 ? i+= 1 : i)
+var moveLeft = key => (i > 0 ? i-= 1 : i)
+var moveUp = key => (j > 0 ? j-= 1 : j)
+var moveDown = key => (j < 4 ? j+= 1 : j)
+
+// document.onkeydown = function(key) {
+//   switch (key.keyCode) {
+//     case 37: position.moveLeft();
+//     break;
+//     case 39: position.moveRight();
+//     break;
+//     case 38: position.moveUp();
+//     break;
+//     case 40: position.moveDown();
+//     break;
+//   }
+//   updatePosition();
+// };
+
