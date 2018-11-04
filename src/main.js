f22fc6e284a86e502475dac49bd42fb48dc39a29
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-04 18:11:23 +0100

    problem with : hiding the start and end points for the 3 first seconds

--- src/main.js
+++ src/main.js
@@ -27,8 +27,17 @@ function matrixCreate (board) {
     for (var j= 0; j < board[i].length; j++){
       var cell = $('<div class="cell ' + board[i][j] +'"> </div>');
       row.append(cell);
+      
+      // show lights after 5sec : PROBLEM HERE => DOESN'T WORK
+      if (cell.hasClass("cell light")){
+        $(".light").toggle();
+        setTimeout(function() {
+          $(".light").toggle();
+        }, 2000);
+      }
+
       // hide bombs after 5sec
-      if (cell.hasClass("bomb")){
+      if (cell.hasClass("cell bomb")){
         setTimeout(function(){
           $(".cell").removeClass("bomb");         
         }, 2000);
