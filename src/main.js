fd1dccf6191f26cc8d8a7a6fb9bb00a3597a103c
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-06 21:06:26 +0100

    Game working ! Needed : add score, add sets, add player

--- src/main.js
+++ src/main.js
@@ -11,67 +11,103 @@ var board = [
   [free, free, free, bomb, free]
 ];
 
-
-$(document).ready(function(){
+$(document).ready(function() {
   matrixCreate(board);
-})
+
+  // HOW TO MOVE
+  document.onkeydown = function(key) {
+    if (!$(".light").hasClass("used")) {
+      switch (key.keyCode) {
+        case 37:
+          moveLeft();
+          break;
+        case 39:
+          moveRight();
+          break;
+        case 38:
+          moveUp();
+          break;
+        case 40:
+          moveDown();
+          break;
+      }
+    }
+    // UPDATE CELL COLOR when changing cell
+    var cell = $(".row-" + i + ".col-" + j);
+    cell.addClass("used").removeClass("hidden");
+
+    // MOVES DISABLES IF THE GAME ENDS
+    if ($(".light").hasClass("used")) {
+      alert("partie terminée!");
+      // moves not possible
+      checkPath();
+    }
+  };
+});
 
 //__________ CREATE A MATRIX WITH BOMBS, END/START POINTS ___________________
 
 var matrixCreate = board => {
-  for (var i= 0; i < board.length; i++){
+  for (var i = 0; i < board.length; i++) {
     var row = $("<div></div>");
     $("#game_board").append(row);
 
-    for (var j= 0; j < board[i].length; j++){
-      var cell = $('<div class="cell ' + board[i][j] +'"> </div>');
+    for (var j = 0; j < board[i].length; j++) {
+      var cell = $('<div class="cell ' + board[i][j] + '"> </div>');
+      cell.addClass("row-" + i).addClass("col-" + j);
       row.append(cell);
-      
-      // // show lights after 5sec : PROBLEM HERE => TOGGLE NOT WORKING
-      // if (cell.hasClass("light")){
-      //   setTimeout(function() {
-      //     $(".light").addClass("hidden");
-      //   }, 2000);
-      // }
-
-      // hide bombs after 5sec
-      if (cell.hasClass("bomb")){
-        setTimeout(function(){
-          $(".bomb").addClass("hidden");         
-        }, 3000);
-      }
-
     }
-  }
-}
 
+    // show lights after 3sec :
+    $(".light").addClass("hidden");
+    setTimeout(function() {
+      $(".light").removeClass("hidden");
+    }, 3000);
+
+    // hide bombs after 5sec
+    setTimeout(function() {
+      $(".bomb").addClass("hidden");
+    }, 3000);
+  }
+};
 
 // _____________ FUNCTIONS FOR MOVING _____________________________________
 
-var i= 1;
-var j= 0; 
+var i = 1;
+var j = 0;
 var position = board[i][j];
 
+var moveRight = key => (j < 4 ? (j += 1) : j);
+var moveLeft = key => (j > 0 ? (j -= 1) : j);
+var moveUp = key => (i > 0 ? (i -= 1) : i);
+var moveDown = key => (i < 4 ? (i += 1) : i);
 
-var moveRight = key => (j < 4 ? j+= 1 : j)
-var moveLeft = key => (j > 0 ? j-= 1 : j)
-var moveUp = key => (i > 0 ? i-= 1 : i)
-var moveDown = key => (i < 4 ? i+= 1 : i)
+// _____________ CHECKING IF THE PATH WAS PERILOUS __________________
 
-document.onkeydown = function(key) {
-  switch (key.keyCode) {
-    case 37: moveLeft();
-    break;
-    case 39: moveRight();
-    alert("la nouvelle position est : (" + i + "," + j + ")");
-    break;
-    case 38: moveUp();
-    break;
-    case 40: moveDown();
-    break;
+function checkPath() {
+  // if perilous => show the bombs used
+  if ($(".bomb").hasClass("used")) {
+    $(".bomb.used")
+      .removeClass("used")
+      .addClass("explosion");
+    $(".bomb").removeClass("hidden");
+    $(".light")
+      .removeClass("light")
+      .addClass("all-red");
+    $(".used")
+      .removeClass("used")
+      .addClass("all-red");
   }
-  // UPDATE CELL COLOR => DOESN'T WORK
-  var cell = $('<div class="cell ' + board[i][j] +'"> </div>');
-    cell.addClass("used");
-};
-
+  // if not perilous => all green
+  else {
+    $(".light")
+      .removeClass("light")
+      .removeClass("used")
+      .addClass("all-green");
+    $(".used")
+      .removeClass("light")
+      .removeClass("used")
+      .addClass("all-green");
+    $(".bomb").removeClass("hidden");
+  }
+}
