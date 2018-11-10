4d0a953b3cc645f0dd75e789846b708ba5666166
Rita Mernissi <mernissi.rita@gmail.com>
2018-11-10 10:45:40 +0100

    last commit before breaking everything

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 var bomb = "bomb";
 var light = "light";
 var free = "free";
-var scorePlayer= 0;
+var scorePlayer = 0;
+var nbMatch = 0;
+var currentPlayer = 1;
+
 
 var board = [
   [free, free, free, bomb, bomb],
@@ -84,7 +87,7 @@ var moveDown = key => (i < 4 ? (i += 1) : i);
 // _____________ CHECKING IF THE PATH WAS PERILOUS OR NOT__________________
 
 function checkPath() {
-  // if perilous => show the bombs used
+  // IF PERILOUS => show the bombs used
   if ($(".bomb").hasClass("used")) {
     $(".bomb.used")
       .removeClass("used")
@@ -97,10 +100,11 @@ function checkPath() {
       .removeClass("used")
       .addClass("all-red");
     // Show the loser popup
-    $(".popup, .popup-content").addClass("active");
-    endMatch();
+    setTimeout(function() {
+      $(".popup, .popup-content").addClass("active");
+    }, 1000);
   }
-  // if not perilous => all green
+  // IF SAFE  => all green
   else {
     $(".light")
       .removeClass("light")
@@ -112,30 +116,41 @@ function checkPath() {
       .addClass("all-green");
     $(".bomb").removeClass("hidden");
     // edit the score
-    scorePlayer= scorePlayer + 1;
-    var cell= $("#score-player");
+    scorePlayer = scorePlayer + 1;
+    var cell = $("#score-player");
     cell.text(scorePlayer);
     // show the winner popup
-    var contentText= $(".title");
-    var contentGif= $(".gif");
-    var contentButton= $("button");
-    contentGif.attr("src", 'https://giphy.com/embed/3o7bu57lYhUEFiYDSM');
+    var contentText = $(".title");
+    var contentGif = $(".gif");
+    var contentButton = $("button");
+    contentGif.attr("src", "https://giphy.com/embed/3o7bu57lYhUEFiYDSM");
     contentText.text("Winner popup");
     contentButton.text("I am a winner");
-    $(".popup, .popup-content").addClass("active");
-    endMatch();
+    setTimeout(function() {
+      $(".popup, .popup-content").addClass("active");
+    }, 1000);
+  }
+  // Fermer la popup
+  $(".close").on("click", function() {
+    $(".popup, .popup-content").removeClass("active");
+  });
+  nbMatch = nbMatch + 1;
+  nextMatch();
+}
+
+/*_____________________STARTING ENDING A MATCH_____________________ */
+function nextMatch() {
+  if (nbMatch > 10) {
+    alert("the winner is one of the 2 players. Deal with it.")
+  } 
+  else {
+    (currentPlayer === 1 ? currentPlayer = 2 : currentPlayer = 1);
   }
 }
 
-/*_____________________TARTING ENDING A MATCH_____________________ */
-function endMatch() {
-  // afficher une popup (winner ou loser)
-  $(".close").on("click", function(){
-    $(".popup, .popup-content").removeClass("active");
-  });
-  
+/* 2 joueurs : 
 
-  // si le nombre de tableau joués est 5 = annonce le gagnant
+- var current player 
+si player 1 qui vient de jouer et a terminé alors redessine le jeu et mon current player devient player 2 et donc c'est le score de current player qui doit être incrémenté. 
 
-  // si le nombre de tableaux joués est < 5 = afficher un nouveau tableau
-}
+*/
